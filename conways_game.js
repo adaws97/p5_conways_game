cf8cc5553775deff5947b6bd58e546c7d1d8f8b7
Alexander Dawson <alexander_dawson@ymail.com>
2017-03-16 16:10:58 +0000

    Add functionality for randomly adding pre-made structures to the grid

--- conways_game.js
+++ conways_game.js
@@ -1,10 +1,4 @@
-var cellSize = 5;
-
-var startLiveProbability = 15;
-
-var updateTime = 1000;
-var lastTime = 0;
-
+var cellSize;
 var alive,
     dead;
 
@@ -12,24 +6,25 @@ var cells,
     cellBuffer;
 
 function setup() {
-  alive = color(0, 255, 0);
+  alive = color(0, 190, 0);
   dead = color(0);
-
-  createCanvas(720, 400);
+  createCanvas(windowWidth, windowHeight);
+  cellSize = 10;
   createCellArray();
-  initRandomCells();
+  initRandomEntities();
   stroke(48);
   noSmooth();
+  frameRate(2);
   background(0);
 }
 
 function createCellArray() {
   cells = [];
   cellBuffer = [];
-  for (var x = 0; x < width / cellSize; x++) {
+  for (var x = 0; x < width / cellSize; x += 1) {
     cells[x] = [height / cellSize];
     cellBuffer[x] = [height / cellSize];
-    for (var y = 0; y < height / cellSize; y++) {
+    for (var y = 0; y < height / cellSize; y += 1) {
       cells[x][y] = 0;
       cellBuffer[x][y] = 0;
     }
@@ -37,8 +32,9 @@ function createCellArray() {
 }
 
 function initRandomCells() {
-    for (var x = 0; x < width / cellSize; x++) {
-      for (var y = 0; y < height / cellSize; y++) {
+  var startLiveProbability = 1;
+  for (var x = 0; x < width / cellSize; x += 1) {
+    for (var y = 0; y < height / cellSize; y += 1) {
       var state = random(100);
       if (state > startLiveProbability) {
         cells[x][y] = 0;
@@ -49,12 +45,130 @@ function initRandomCells() {
   }
 }
 
+function initRandomEntities() {
+  var chanceToCreate = 1;
+  for (var x = 0; x < width / cellSize; x += 1) {
+    for (var y = 0; y < height / cellSize; y += 1) {
+      var random = Math.random() * 500;
+      if (random < chanceToCreate) {
+        var entityChoice = floor(Math.random() * 6);
+        switch (entityChoice) {
+          case 0:
+            placeBeaconAt(x, y);
+            break;
+          case 1:
+            placeHorizontalBlinkerAt(x, y);
+            break;
+          case 2:
+            placeLoafAt(x, y);
+            break;
+          case 3:
+            placeBeehiveAt(x, y);
+            break;
+          case 4:
+            placeToadAt(x, y);
+            break;
+          case 5:
+            placeBlockAt(x, y);
+            break;
+          case 6:
+            placePentadecathlonAt(x, y);
+            break;
+          default:
+          break;
+        }
+      }
+    }
+  }
+}
+
+function placeBlockAt(x, y) {
+  if (x + 1 < width / cellSize &&
+      y + 1 < height / cellSize) {
+    cells[x][y] = 1;
+    cells[x + 1][y] = 1;
+    cells[x][y + 1] = 1;
+    cells[x + 1][y + 1] = 1;
+  }
+}
+
+function placeBeehiveAt(x, y) {
+  if (x + 3 < width / cellSize &&
+      y + 2 < height / cellSize) {
+    cells[x + 1][y] = 1;
+    cells[x + 2][y] = 1;
+    cells[x][y + 1] = 1;
+    cells[x + 3][y + 1] = 1;
+    cells[x + 1][y + 2] = 1;
+    cells[x + 2][y + 2] = 1;
+  }
+}
+
+function placeLoafAt(x, y) {
+  if (x + 3 < width / cellSize &&
+      y + 2 < height / cellSize) {
+    cells[x + 1][y] = 1;
+    cells[x + 2][y] = 1;
+    cells[x][y + 1] = 1;
+    cells[x + 3][y + 1] = 1;
+    cells[x + 1][y + 2] = 1;
+    cells[x + 3][y + 2] = 1;
+    cells[x + 2][y + 2] = 1;
+  }
+}
+
+function placeHorizontalBlinkerAt(x, y) {
+  if (x + 2 < width / cellSize) {
+    cells[x][y] = 1;
+    cells[x + 1][y] = 1;
+    cells[x + 2][y] = 1;
+  }
+}
+
+function placeVerticalBlinkerAt(x, y) {
+  if (y + 2 < height / cellSize) {
+    cells[x][y] = 1;
+    cells[x][y + 1] = 1;
+    cells[x][y + 2] = 1;
+  }
+}
+
+function placeToadAt(x, y) {
+  if (x + 3 < width / cellSize &&
+      y + 1 < height / cellSize) {
+    cells[x + 1][y] = 1;
+    cells[x + 2][y] = 1;
+    cells[x + 3][y] = 1;
+    cells[x][y + 1] = 1;
+    cells[x + 1][y + 1] = 1;
+    cells[x + 2][y + 1] = 1;
+  }
+}
+
+function placeBeaconAt(x, y) {
+  placeBlockAt(x, y);
+  placeBlockAt(x + 2, y + 2);
+}
+
+function placePentadecathlonAt(x, y) {
+  if (x + 3 < width / cellSize &&
+      y + 6 < height / cellSize) {
+    placeHorizontalBlinkerAt(x, y);
+    cells[x + 1][y + 1] = 1;
+    cells[x + 3][y + 1] = 1;
+    placeHorizontalBlinkerAt(x, y + 2);
+    placeHorizontalBlinkerAt(x, y + 3);
+    placeHorizontalBlinkerAt(x, y + 4);
+    placeHorizontalBlinkerAt(x, y + 5);
+    cells[x + 1][y + 6] = 1;
+    cells[x + 3][y + 6] = 1;
+    placeHorizontalBlinkerAt(x, y + 7);
+  }
+}
+
 function draw() {
   drawCells();
-  if (millis() - lastTime > updateTime) {
-    updateCells();
-    lastTime = millis();
-  }
+  updateCells();
 }
 
 function drawCells() {
@@ -99,11 +213,11 @@ function saveCells() {
 
 function getNeighboursFor(cellX, cellY) {
   var neighbours = 0;
-  for (var x = cellX - 1; x < cellX + 1; x += 1) {
-    for (var y = cellY - 1; y < cellY + 1; y += 1) {
+  for (var x = cellX - 1; x <= cellX + 1; x += 1) {
+    for (var y = cellY - 1; y <= cellY + 1; y += 1) {
       if (((x >= 0) && (x < width / cellSize)) &&
           ((y >= 0) && (y < height / cellSize))) {
-        if (!(x === cellX) && !(y === cellY)) {
+        if (!((x == cellX) && (y == cellY))) {
           if (cellBuffer[x][y] === 1) {
             neighbours += 1;
           }
